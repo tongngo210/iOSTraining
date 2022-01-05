03ef0ad53e1c48728424136b0ec0756a7ff51086
tongngo210 <tong.ngo@madison-technology.com>
2022-01-05 10:08:39 +0700

    Refactor code

--- Swift Basics/SwiftBasicsPractice.playground/Contents.swift
+++ Swift Basics/SwiftBasicsPractice.playground/Contents.swift
@@ -1,49 +1,40 @@
 import Foundation
 
 // Viết function chuyển số thập phân thành số nhị phân
-func changeToBinaryNum(from decimalNumber: Int) -> String {
-    var result = ""
+func changeToBinaryNum(from decimalNumber: Int) {
+    var result = [String]()
     var num = decimalNumber
-    while num > 0 {
-        switch num % 2 {
-        case 0:
-            result += "0"
-        case 1:
-            result += "1"
-        default:
-            result += "1"
+    if num <= 0 {
+        print("Your input must be greater than 0! Try again")
+    } else {
+        while num > 0 {
+            result += [num].map {$0 % 2 == 0 ? "0" : "1"}
+            num /= 2
         }
-        num /= 2
+        print(result.reversed().joined())
     }
-    return String(result.reversed())
 }
 changeToBinaryNum(from: 33)
 
 /* Viết chương trình cho phép nhập vào một số nguyên dương n,
  Tính tổng tất cả số chẵn trong khoảng từ 0 - n. */
-func sumEvenNumberFromZero(to number: Int) -> String {
-    if number < 0 {
-        return "Your input must be plus! Try again"
+func sumEvenNumberFromZero(to number: Int) {
+    if number <= 0 {
+        print("Your input must be greater than 0! Try again")
+    } else {
+        let result = (0...number)
+            .filter { $0 % 2 == 0 }
+            .reduce(0, +)
+        print("Your result is \(result)")
     }
-    var result = 0
-    for i in 0...number {
-        if i % 2 == 0 {
-            result += i
-        }
-    }
-    return "Your result is \(result)"
 }
-
-print(sumEvenNumberFromZero(to: 5))
+sumEvenNumberFromZero(to: 5)
 
 /* Viết chương trình để nhập nhập một số nguyên,
  Tìm kết quả phép nhân của số đó với các số từ 1 - 20 , sau đó in kết quả ra màn hình.
  */
 func multiplicationFromOneToTwenty(of number: Int) {
-    var result = [Int]()
-    for i in 1...20 {
-        result += [number * i]
-    }
+    let result = (1...20).map { $0 * number }
     print(result)
 }
 multiplicationFromOneToTwenty(of: 2)
@@ -53,27 +44,15 @@ multiplicationFromOneToTwenty(of: 2)
  */
 func findPrimeNumbersFromZero(to number: Int) {
     func isPrime(num: Int) -> Bool {
-        switch num {
-        case num where num == 2, 3:
-            return true
-        default:
-            let maxDivider = Int(Double(num).squareRoot())
-            print(maxDivider)
-            return !(2...maxDivider).contains { num % $0 == 0 }
-        }
+        let maxDivider = Int(Double(num).squareRoot())
+        return num == 2 || num == 3 ? true :
+        !(2...maxDivider).contains { num % $0 == 0 }
     }
-    isPrime(num: 3)
-    switch number {
-    case number where number > 1000 || number <= 1:
+    
+    if number > 1000 || number <= 1 {
         print("Your input must less than 1000 and greater than 1")
-    default:
-        var result = [Int]()
-        for n in 2...number {
-            if isPrime(num: n) {
-                result += [n]
-            }
-        }
-        print(result)
+    } else {
+        print((2...number).filter { isPrime(num: $0) == true })
     }
 }
 findPrimeNumbersFromZero(to: 5)
@@ -86,20 +65,12 @@ func findFibonacciNumbers(number: Int) {
         print("Number must greater than 0 and less than 20")
     } else {
         var result = [Int]()
-        switch number {
-        case 1:
-            result = [1]
-        case 2:
-            result = [1,1]
-        default:
-            var n1 = 1
-            var n2 = 1
-            result += [n1, n2]
-            for _ in 3...number {
-                let n3 = n1 + n2
-                result += [n3]
-                n1 = n2
-                n2 = n3
+        if number == 1 || number == 2 {
+            (1...number).forEach { _ in result += [1] }
+        } else {
+            result = [1, 1]
+            (0...number - 3).forEach { num in
+                result.append(result[num] + result[num + 1])
             }
         }
         print(result)
